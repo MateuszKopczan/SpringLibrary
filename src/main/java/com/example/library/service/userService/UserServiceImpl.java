c75c8752fcba28aa20ad439c9515525587deaa51
Mateusz <61706186+MatikX98@users.noreply.github.com>
2022-06-01 13:51:08 +0200

    Update UserServiceImpl.java

--- src/main/java/com/example/library/service/userService/UserServiceImpl.java
+++ src/main/java/com/example/library/service/userService/UserServiceImpl.java
@@ -60,6 +60,7 @@ public class UserServiceImpl implements UserService{
         user.setUserName(userDTO.getUserName());
         user.setPassword(encodedPassword);
         user.setEmail(userDTO.getEmail());
+        user.setCart(new Cart());
         user.setVerificationCode(userDTO.getVerifyCode());
         user.setRoles(List.of(roleDAO.findRoleByName("ROLE_USER")));
         userDAO.save(user);
